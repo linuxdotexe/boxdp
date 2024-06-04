2fb8a3fcce30db1279989f284fc1387846c4aec0
sai nivas mangu <sainonbeat99@gmail.com>
2024-06-04 11:37:03 +0530

    fix: useSearchParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useState, useRef, Suspense } from "react";
 
 import ImageViewer from "@/components/ImageViewer";
 import SearchBox from "@/components/SearchBox";
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 /*
 import dynamic from "next/dynamic";
 const ImageViewer = dynamic(() => import('@/components/ImageViewer'), {
@@ -22,21 +22,18 @@ const IMAGE_URL =
 const DEFAULT_REVIEW =
   "https://letterboxd.com/raybean/film/puss-in-boots-the-last-wish/";
 
-
 export default function Home() {
   // const [apiData, setApiData] = useState<ApiData | null>(null);
   const [isFetching, setIsFetching] = useState(false);
 
-
-
-
   return (
     <div className="flex-1 flex flex-col align-center items-center justify-center bg-gradient-to-r from-orange-950 via-green-950 to-blue-950">
-
       <SearchBox isFetching={isFetching} />
       <div className="flex flex-col justify-evenly" id="review">
-        <ImageViewer isFetching={isFetching} setIsFetching={setIsFetching} />
+        <Suspense>
+          <ImageViewer isFetching={isFetching} setIsFetching={setIsFetching} />
+        </Suspense>
       </div>
     </div>
   );
-};
+}
