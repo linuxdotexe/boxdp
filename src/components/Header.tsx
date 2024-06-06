ea56e048cb0a28e36e4f758f4f5bdd80c4fcf06e
sai nivas mangu <sainonbeat99@gmail.com>
2024-06-06 21:35:37 +0530

    fix: logo to link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full bg-neutral-900 p-2.5">
-      <Image
-        src="/logo.png"
-        width={50}
-        height={50}
-        alt="Logo"
-        className="m-auto"
-      />
+      <Link href="/">
+        <Image
+          src="/logo.png"
+          width={50}
+          height={50}
+          alt="Logo"
+          className="m-auto"
+        />
+      </Link>
     </header>
   );
 }
